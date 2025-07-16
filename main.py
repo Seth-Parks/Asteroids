de9f5da5f8a5db41972672877e0afadf6d0cd32d
Seth-Parks <bparksghub@gmail.com>
2025-07-16 16:32:41 -0500

    Added the ability to move forward and backwards

--- main.py
+++ main.py
@@ -24,10 +24,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+            #fill the screen with black
             screen.fill(color=(0,0,0,))
-
+            
+            
+            
+            #draw the player to the screen
             player.draw(screen)
 
+
+
+
+            #update the player's location then calulated the delta
             player.update(dt)
             dt = clock.tick(60) / 1000
             pygame.display.flip()
