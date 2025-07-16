70b353386c873fd2df422f7a2dc8f7aebafd8e64
Seth-Parks <bparksghub@gmail.com>
2025-07-16 16:15:08 -0500

    Added a circleshape class, new player class, and implemented them into main.

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
+# Author: Brandon Parks
+# Project Name: Asteroids
+
 import pygame
-#Importing wildcard constants, since the project is relatively small.
+#Importing wildcard, since the project is relatively small.
 from constants import *
+from circleshape import *
+from player import *
 
 
 def main():
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             screen.fill(color=(0,0,0,))
+
+            player.draw(screen)
+
+            player.update(dt)
+            dt = clock.tick(60) / 1000
             pygame.display.flip()
 
             
